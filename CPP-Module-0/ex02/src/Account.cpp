bf32c03e279dccc338fbf5f29c02066acfa8ef3d
dmarceli <daniel.sequeira@skeyra.pt>
2023-01-23 00:10:02 +0000

    finished module 00

--- CPP-Module-0/ex02/src/Account.cpp
+++ CPP-Module-0/ex02/src/Account.cpp
@@ -1,14 +1,10 @@
 #include "Account.hpp"
 #include <string>
 #include <iostream>
-#include <cstdlib>
-#include <sstream>
 #include <iomanip>
 typedef std::string	str;
 
-
 //constructors and destructors
-
 
 int	Account::_nbAccounts = 0;
 int	Account::_totalAmount = 0;
@@ -27,22 +23,22 @@ int Account::checkAmount(void) const{return Account::_amount;}
 
 Account::Account(int initial_deposit)
 {
-	this->_accountIndex = Account::_nbAccounts++;
-	this->_amount = initial_deposit;
-	this->_nbDeposits = 0;
-	this->_nbWithdrawals = 0;
-	Account::_totalAmount += this->_amount;
+	_accountIndex = _nbAccounts++;
+	_amount = initial_deposit;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	Account::_totalAmount += _amount;
 	Account::_displayTimestamp();
-	std::cout << "index:" << this->_accountIndex << ";"
-				<< "amount:" << this->_amount << ";"
+	std::cout << "index:" << _accountIndex << ";"
+				<< "amount:" << _amount << ";"
 				<< "created" << std::endl;
 }
 
 Account::~Account( void ) {
-	Account::_nbAccounts--;
-	Account::_displayTimestamp();
-	std::cout << "index:" << this->_accountIndex << ";"
-				<< "amount:" << this->_amount << ";"
+	_nbAccounts--;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";"
+				<< "amount:" << _amount << ";"
 				<< "closed" << std::endl;
 }
 
@@ -62,55 +58,55 @@ void Account::_displayTimestamp(void)
 
 void	Account::displayAccountsInfos( void ) 
 {
-	Account::_displayTimestamp();
-	std::cout << "accounts:" << Account::getNbAccounts()
-		    	<< ";total:" << Account::getTotalAmount()
-		    	<< ";deposits:" << Account::getNbDeposits()
-		    	<< ";withdrawals:" << Account::getNbWithdrawals() << std::endl;
+	_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts()
+		    	<< ";total:" << getTotalAmount()
+		    	<< ";deposits:" << getNbDeposits()
+		    	<< ";withdrawals:" << getNbWithdrawals() << std::endl;
 }
 
 void Account::displayStatus(void) const
 {
 	Account::_displayTimestamp();
-	std::cout << "index:" << Account::_accountIndex << ";"
-				<< "amount:" << Account::checkAmount() << ";"
-				<< "deposits:" << Account::_nbDeposits << ";"
-				<< "withdrawals:" << Account::_nbWithdrawals << std::endl;
+	std::cout << "index:" << _accountIndex << ";"
+				<< "amount:" << checkAmount() << ";"
+				<< "deposits:" << _nbDeposits << ";"
+				<< "withdrawals:" << _nbWithdrawals << std::endl;
 }
 
-// deposits and withdrawls
+// deposits and withdrawls methods
 
 void Account::makeDeposit(int amount)
 {
 	Account::_displayTimestamp();
-	std::cout << "index:" << this->_accountIndex
-		    	<< ";p_amount:" << this->_amount
+	std::cout << "index:" << _accountIndex
+		    	<< ";p_amount:" << _amount
 		    	<< ";deposit:" << amount;
 	
-	this->_amount += amount;
+	_amount += amount;
 	_totalAmount += amount;
 
 	_nbDeposits++;
-	this->_totalNbDeposits++;
+	_totalNbDeposits++;
 
-	std::cout << ";amount:" << this->_amount
-				<< ";nb_deposits:" << this->_nbDeposits << std::endl;
+	std::cout << ";amount:" << _amount
+				<< ";nb_deposits:" << _nbDeposits << std::endl;
 }
 
 bool	Account::makeWithdrawal( int withdrawal ) 
 {
 
-	Account::_displayTimestamp();
-	std::cout << "index:" << this->_accountIndex
-		      << ";p_amount:" << this->_amount;
-	if (this->_amount >= withdrawal) {
-		this->_amount -= withdrawal;
-		this->_nbWithdrawals++;
-		Account::_totalNbWithdrawals++;
-		Account::_totalAmount -= withdrawal;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex
+		      << ";p_amount:" << _amount;
+	if (_amount >= withdrawal) {
+		_amount -= withdrawal;
+		_nbWithdrawals++;
+		_totalNbWithdrawals++;
+		_totalAmount -= withdrawal;
 		std::cout << ";withdrawal:" << withdrawal
-					<< ";amount:" << this->_amount
-					<< ";nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+					<< ";amount:" << _amount
+					<< ";nb_withdrawals:" << _nbWithdrawals << std::endl;
 		return (true);
 	}
 	std::cout << ";withdrawal:refused" << std::endl;
