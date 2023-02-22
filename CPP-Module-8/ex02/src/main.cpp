89c636af02fe15947385b5241ba9ddbfcfa51a42
Daniel Marcelino sequeira <dmarceli@c2r1s8.42lisboa.com>
2023-02-22 16:42:16 +0000

    Finito

--- CPP-Module-8/ex02/src/main.cpp
+++ CPP-Module-8/ex02/src/main.cpp
@@ -2,25 +2,51 @@
 
 int	main(void)
 {
-	MutantStack<int> mstack;
-	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
-	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	mstack.push(0);
-	MutantStack<int>::iterator it = mstack.begin();
-	MutantStack<int>::iterator ite = mstack.end();
-	++it;
-	--it;
-	while (it != ite)
 	{
-		std::cout << *it << std::endl;
+		MutantStack<int> mstack;
+		mstack.push(5);
+		mstack.push(17);
+		std::cout << mstack.top() << std::endl;
+		mstack.pop();
+		std::cout << mstack.size() << std::endl;
+		mstack.push(3);
+		mstack.push(5);
+		mstack.push(737);
+		mstack.push(0);
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
 		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+		std::stack<int> s(mstack);
 	}
-	std::stack<int> s(mstack);
+	std::cout << "===|2ND TEST|==="<< std::endl;
+	{
+		MutantStack<std::string> mstack_str;
+
+		mstack_str.push("one");
+		mstack_str.push("two");
+		mstack_str.push("three");
+		mstack_str.push("four");
+		mstack_str.push("five");
+		MutantStack<std::string>::iterator it = mstack_str.begin();
+		MutantStack<std::string>::iterator ite = mstack_str.end();
+
+		std::cout << mstack_str.top() << std::endl;
+		std::cout << mstack_str.size() << std::endl;
+	
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
 	return (0);
+
 }
