3cc35dc862b941e9603b5cab60729464f19a283f
dmarceli <danie.sequeira@skeyra.pt>
2023-03-23 16:15:57 +0000

    algo done for list

--- CPP-Module-9/ex02/incs/PmergeMe.hpp
+++ CPP-Module-9/ex02/incs/PmergeMe.hpp
@@ -6,6 +6,10 @@
 # include <cstdlib>
 # include <vector>
 # include <iterator>
+# include <sys/time.h>
+# include <algorithm>
+# include <list>
+
 class PmergeMe
 {
 	public:
@@ -15,12 +19,14 @@ class PmergeMe
 		PmergeMe &operator=( PmergeMe const & rhs );
 		void error(){std::cout << "Error\n";exit(0);}
 		void load_vector(char **av);
-		std::vector<unsigned int> merge_vec(std::vector<unsigned int> v);
+		std::vector<unsigned int> merge_vec(std::vector<unsigned int> &v);
+		std::list<unsigned int> merge_lst(std::list<unsigned int> &lst);
 		std::vector<unsigned int> merge(const std::vector<unsigned int>& left, const std::vector<unsigned int>& right);
-
+		std::list<unsigned int> merge(const std::list<unsigned int>& left, const std::list<unsigned int>& right);
 	private:
 		PmergeMe();
 		std::vector<unsigned int> _vec;
+		std::list<unsigned int> _lis;
 		unsigned int _vecsize;
 };
 
--- CPP-Module-9/ex02/src/PmergeMe.cpp
+++ CPP-Module-9/ex02/src/PmergeMe.cpp
@@ -1,45 +1,53 @@
 #include "PmergeMe.hpp"
 
-void print(std::vector<unsigned int> const &input)
+void print(std::vector<unsigned int> const &input, int indicator)
 {
-    for (size_t i = 0; i < input.size(); i++) 
-	{
-		std::cout << input.at(i);
-		if (i != input.size() - 1)
-        	std::cout << ',';
-		else
-			std::cout << '\n';
-    }
+	if (indicator)
+		std::cout << "After: ";
+	else
+		std::cout << "Before: ";
+	size_t i ;
+    for (i = 0; i < input.size(); i++) 
+		std::cout << input.at(i) << " ";
+	std::cout << std::endl;
+	
+}
+
+long double get_time(){
+    timeval tv;
+
+    gettimeofday(&tv, NULL);
+	unsigned long micro = tv.tv_usec*(u_int64_t)1000000+tv.tv_usec;
+    return micro  / 1000000;
 }
 
 PmergeMe::PmergeMe(){}
 
 PmergeMe::PmergeMe(int ac, char **av): _vecsize(ac - 1)
 {
-	(void)av;
+	long double vectime;
+	long double listime;
+
 	if(_vecsize < 3)
 		error();
-	//load_vector(av);
-	_vec.push_back(1);
-	_vec.push_back(6);
-	_vec.push_back(2);
-	_vec.push_back(77);
-	_vec.push_back(7);
-	_vec.push_back(8);
-	print(_vec);
+	load_vector(av);
+	print(_vec, 0);
+	vectime = get_time();
 	_vec = merge_vec(_vec);
-	print(_vec);
+	vectime = get_time() - vectime;
+	print(_vec, 1);
+	std::cout << "Time to process a range of " << _vecsize << " with std::vector : "<< vectime << "µs\n";
+	listime = get_time();
+	_lis = merge_lst(_lis);
+	listime = get_time() - listime;
+	std::cout << "Time to process a range of " << _vecsize << " with std::list : "<< listime << "µs\n";
 }
 
-PmergeMe::PmergeMe( const PmergeMe & src )
-{
-	*this = src;
-}
+PmergeMe::PmergeMe( const PmergeMe & src ){*this = src;}
 
 PmergeMe::~PmergeMe()
 {
 }
-
 
 PmergeMe &				PmergeMe::operator=( PmergeMe const & rhs )
 {
@@ -48,7 +56,20 @@ PmergeMe &				PmergeMe::operator=( PmergeMe const & rhs )
 	return *this;
 }
 
-std::vector<unsigned int> PmergeMe::merge_vec(std::vector<unsigned int> v)
+std::list<unsigned int> PmergeMe::merge_lst(std::list<unsigned int> &lst)
+{
+	if (lst.size() == 1)
+        return lst;
+    std::list<unsigned int>::iterator it = lst.begin();
+    std::advance(it, lst.size() / 2);
+    std::list<unsigned int> left(lst.begin(), it);
+    std::list<unsigned int> right(it, lst.end());
+    left = merge_lst(left);
+    right = merge_lst(right);
+	return merge(left, right);
+}
+
+std::vector<unsigned int> PmergeMe::merge_vec(std::vector<unsigned int> &v)
 {
 	if (v.size() == 1)
         return v;
@@ -60,6 +81,25 @@ std::vector<unsigned int> PmergeMe::merge_vec(std::vector<unsigned int> v)
 	return merge(left, right);
 }
 
+std::list<unsigned int> PmergeMe::merge(const std::list<unsigned int>& left, const std::list<unsigned int>& right)
+{
+    std::list<unsigned int> result;
+    std::list<unsigned int>::const_iterator it_left = left.begin();
+    std::list<unsigned int>::const_iterator it_right = right.begin();
+    while (it_left != left.end() && it_right != right.end())
+    {
+        if (*it_left <= *it_right)
+            result.push_back(*it_left++);
+        else
+            result.push_back(*it_right++);
+    }
+    while (it_left != left.end())
+        result.push_back(*it_left++);
+    while (it_right != right.end())
+        result.push_back(*it_right++);
+    return result;
+}
+
 std::vector<unsigned int> PmergeMe::merge(const std::vector<unsigned int>& left, const std::vector<unsigned int>& right)
 {
     std::vector<unsigned int> result(left.size() + right.size());
@@ -67,32 +107,23 @@ std::vector<unsigned int> PmergeMe::merge(const std::vector<unsigned int>& left,
     while (i < left.size() && j < right.size()) 
 	{
         if (left[i] <= right[j]) 
-		{
-            result[k] = left[i];
-            i++;
-        } 
+            result[k] = left[i++];
 		else 
-		{
-            result[k] = right[j];
-            j++;
-        }
+            result[k] = right[j++];
         k++;
     }
-    while (i < left.size()) {
-        result[k] = left[i];
-        i++;
-        k++;
-    }
-    while (j < right.size()) {
-        result[k] = right[j];
-        j++;
-        k++;
-    }
+    while (i < left.size()) 
+        result[k++] = left[i++];
+    while (j < right.size())
+        result[k++] = right[j++];
    return result;
 }
 
 void PmergeMe::load_vector(char **av)
 {
-    for (unsigned int i = 1; i <= _vecsize; i++) 
+    for (unsigned int i = 1; i <= _vecsize; i++)
+	{
 		_vec.push_back(std::atoi(av[i]));
+		_lis.push_back(std::atoi(av[i]));
+	}
 }
